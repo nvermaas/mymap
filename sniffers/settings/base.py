0c80e6a2b321df0715bae2961110aa22d36ed320
Vermaas <vermaas@astron.nl>
2023-11-26 08:18:41 +0100

    ci/cd pipeline

--- sniffers/settings/base.py
+++ sniffers/settings/base.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-m$(%@hdu-rv&6c6+q)pnex_vpss@@y!nfy$15%jy1z(d38k+i&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
--- sniffers/settings/docker.py
+++ sniffers/settings/docker.py
@@ -1,2 +1,3 @@
 from sniffers.settings.base import *
 DEBUG = True
+ALLOWED_HOSTS = ['*']
